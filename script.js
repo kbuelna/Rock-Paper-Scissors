64f1eb2b587ec69bbc46536844960f1e40cffbe1
kbuelna <andreabuelna@hotmail.com>
2021-06-08 23:13:41 -0400

    Update script.js
    added singleRound function and computer and player variables

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function computerPlay() {
     return computerChoice;
 }
 
+let computerSelection = computerPlay();
+let playerSelection = "Paper";
+
+function singleRound(player, computer) {
+    let message = "";
+    if (playerSelection == computerSelection) {
+        message = "It's a tie!";
+    } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Paper" && computerSelection == "Rock") || (playerSelection == "Scissors" && computerSelection == "Paper")) {
+        message = "You won! " + playerSelection + " beats " + computerSelection;
+    } else {
+        message = "You lost! " + computerSelection + " beats " + playerSelection;
+    }
+
+    return message;
+}
+
+
