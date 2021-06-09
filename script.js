4458de37d697e4ae4159166a3ea02af0c09be334
kbuelna <andreabuelna@hotmail.com>
2021-06-09 00:13:27 -0400

    Update script.js
    Added score counters, and game function

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-let options = ["Rock", "Paper", "Scissors"];
+let playerSelection = window.prompt("Rock, Paper or Scissors?");
+
+function selections() {
+   playerSelection = window.prompt("Rock, Paper or Scissors?");
+   computerSelection = computerPlay();
+}
+
 
 function computerPlay() {
+    let options = ["Rock", "Paper", "Scissors"];
     let computerChoice = options[Math.floor(Math.random() * 3)];
     return computerChoice;
 }
 
 let computerSelection = computerPlay();
-let playerSelection = "Paper";
+let playerScore = 0;
+let computerScore = 0;
 
 function singleRound(player, computer) {
     let message = "";
@@ -14,11 +22,28 @@ function singleRound(player, computer) {
         message = "It's a tie!";
     } else if ((playerSelection == "Rock" && computerSelection == "Scissors") || (playerSelection == "Paper" && computerSelection == "Rock") || (playerSelection == "Scissors" && computerSelection == "Paper")) {
         message = "You won! " + playerSelection + " beats " + computerSelection;
+        playerScore += 1;
     } else {
         message = "You lost! " + computerSelection + " beats " + playerSelection;
+        computerScore += 1;
     }
 
     return message;
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(singleRound(playerSelection, computerSelection));
+        playerSelection = window.prompt("Rock, Paper or Scissors?");
+        computerSelection = computerPlay();
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Congratulations, you won! Final score is You: " + playerScore + " Computer: " + computerScore); 
+    } else if (playerScore < computerScore) {
+        console.log("Oh no, you lost! Final score is You: " + playerScore + " Computer: " + computerScore);
+    } else {
+        console.log("It was a tie! Final score is You: " + playerScore + " Computer: " + computerScore);
+    }
+}
 
